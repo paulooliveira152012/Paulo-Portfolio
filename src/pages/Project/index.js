0f31b9c3480f11dac6108a03327a8e8bfd1cfa31
paulooliveira152012 <paulooliveira152012@gmail.com>
2023-04-29 15:21:35 -0400

    done

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -5,8 +5,9 @@ function Project () {
     return (
         <div class="portfolio" id="portfolio">
           <h2>My Portfolio</h2>
-          <ul>
 
+          <ul>
+            
             <div className="container">
             <a href="https://paulooliveira152012.github.io/runBuddy2/"
             target="_blank"
@@ -17,35 +18,36 @@ function Project () {
             </a>
             </div>
 
-            <li className="runbuddy"> <a
-              href="https://paulooliveira152012.github.io/runBuddy2/"
-              target="_blank"
-              class="project"
-              id="runBuddy"
-              >
-              <div class="title">Run Buddy</div>
+            <div className="container">
+            <a href="https://paulooliveira152012.github.io/horiseon-2-/"
+            target="_blank"
+            class="project"
+            id="horiseon"
+            > <li className="horiseon"></li>
+              <div class="title1">Horiseon</div>
             </a>
-            </li>
-            <li className="horiseon">  <a
-              href="https://paulooliveira152012.github.io/horiseon-2-/"
-              target="_blank"
-              class="project"
-              id="horiseon"
-            >
-              <div class="title">Horiseon</div>
-            </a>
-            </li>
-          
-            <li className="NEE"> <a href="https://radiant-sierra-52768.herokuapp.com/" target="_blank" class="project" id="nee">
-              <div class="title">North Eastern Events</div>
-            </a>
-            </li>
+            </div>
 
-            <li className="listenup"> <a href="https://young-shelf-44589.herokuapp.com/" target="_blank" class="project" id="listenup">
-              <div class="title">ListenUp</div>
+            <div className="container">
+            <a href="https://radiant-sierra-52768.herokuapp.com/"
+            target="_blank"
+            class="project"
+            id="horiseon"
+            > <li className="NEE"></li>
+              <div class="title1">NE Events</div>
             </a>
-            </li>
-          
+            </div>
+
+            <div className="container">
+            <a href="https://young-shelf-44589.herokuapp.com/"
+            target="_blank"
+            class="project"
+            id="horiseon"
+            > <li className="listenup"></li>
+              <div class="title1">ListenUp</div>
+            </a>
+            </div>
+
           </ul>
         </div>
     )
